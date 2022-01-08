79216dc684fac86552eda28815449e06c53525df
Michael Huang <michael@pave.com>
2022-01-07 17:51:23 -0800

    Bump version to 1.1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ README = open("./README.md",'r').read()
 setup(
     name = 'manuf',
     packages = ['manuf'],
-    version = '1.1.3',
+    version = '1.1.4',
     description = 'Parser library for Wireshark\'s OUI database',
     author = 'Michael Huang',
     url = 'https://github.com/coolbho3k/manuf/',
